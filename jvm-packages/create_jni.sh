2471e70f2436fbb6a76a0ca0121b96c07d994c4a
Makoto Yui <myui@apache.org>
2018-01-15 17:20:47 +0900

    Updated script

--- jvm-packages/create_jni.sh
+++ jvm-packages/create_jni.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-echo "build java wrapper"
+
+echo "build java wrapper ..."
+
+if [ -z $CXX ]; then
+  echo 'Environment variable $CXX must be defined'
+  exit 1
+fi
 
 # cd to script's directory
 pushd `dirname $0` > /dev/null
@@ -17,42 +23,6 @@ if [ "`echo $XGBOOST_USE_OMP`" == "1" ]; then
 fi
 
 cd ..
-
-# check CC/CXX options
-arch=$(uname -s)
-
-if [ $arch = 'Darwin' ]; then
-  if [ -z $CC ]; then
-    if [ -x `which gcc-5` ]; then
-      export CC=`which gcc-5`
-    elif [ -x `which gcc-6` ]; then
-      export CC=`which gcc-6`
-    else
-      echo 'export CC=`which gcc-X`; is required.'
-      echo 'Run `brew install gcc-5; export CC=gcc-5;`'
-      exit 1
-    fi
-  fi
-  if [ -z $CXX ]; then
-    if [ -x `which g++-5` ]; then
-       export CXX=`which g++-5`
-    elif [ -x `which g++-6` ]; then
-       export CXX=`which gcc-6`
-    else
-       echo 'export CXX=`which g++-X`; is required.'
-       echo 'Run `brew install g++-5; export CXX=g++-5;`'
-       exit 1
-    fi
-  fi
-else
-  # linux defaults
-  if [ -z $CC ]; then
-      export CC=`which gcc`
-  fi
-  if [ -z $CXX ]; then
-      export CXX=`which g++`
-  fi
-fi
 
 CXX=${CXX} USE_OPENMP=${use_omp} make jvm
 cd jvm-packages
